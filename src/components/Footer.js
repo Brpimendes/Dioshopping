4ee9b0dbffabed1aa452c383489c992f5d096d94
Bruno Mendes <brpimendes@gmail.com>
2022-03-02 23:23:15 -0300

    Criacao da aplicacao dioshopping, foi feito separacao das rotas de contato, home page e criacao dos componentes de Header e footer.

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <hr></hr>
+      <h4 align="center">Made by Bruno Mendes</h4>
+    </div>
+  );
+};
+
+export default Footer;
